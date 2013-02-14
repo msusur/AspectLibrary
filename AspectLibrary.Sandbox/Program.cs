3d6303d156409370b879de5e44b2e456968790d1
Mert Susur <mail@mertsusur.com>
2013-02-14 11:20:47 +0200

    Update AspectLibrary.Sandbox/Program.cs

--- AspectLibrary.Sandbox/Program.cs
+++ AspectLibrary.Sandbox/Program.cs
@@ -12,8 +12,11 @@ namespace AspectLibrary.Sandbox
         {
             var proxy =
                 Proxy<Program>.New()
+                      //If the input parameter is any integer, call my OnBefore Method before the method execution.
                      .To(p => p.Calculate(It.IsAny<int>())).OnBefore(context => Console.WriteLine(context.ClassName))
+                     //If the input parameter is integer and it is 12 then call my OnAfter method after the method is executed.
                      .To(p => p.Calculate(It.Is(12))).OnAfter(context => Console.WriteLine("die"))
+                     //For any input parameter that is an integer, call the OnCatch method whenever an exception is occured.
                      .To(p => p.Calculate(It.IsAny<int>())).OnCatch(context => Console.WriteLine(context.Exception.Message))
                      .Build();
 
